203ef6ff91e3a0ef564401815e00289295956fda
Kenderly Delinois <kenderly.delinois1@outlook.com>
2025-05-20 14:14:48 -0500

    Add project files.

---
+++ Week_7_Assignment_7.1.1/Program.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualBasic;
+using static System.Formats.Asn1.AsnWriter;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography;
+namespace Week_7_Assignment_7._1._1
+{
+    internal class Program
+    {
+        //You are a student who has recently taken an exam with your classmates.
+        //However, the professor has not yet provided the students with a sorted list of exam scores.
+        //To make things easier, you write a program to sort exam scores in ascending order 
+        //using the selection sort algorithm.This way, you can obtain the sorted list of scores 
+        //and see how you performed compared to your classmates.
+        //Also, you choose selection sort since that is an easy way of implementation.
+
+        // Pseudo code
+        // Declare a variable called "temp" to the switch
+        // Declare a variable called "LowestScorePosition to keep track of the first element position
+        // To Conduct Selection Sort start with the first element then compare with next element
+        //  Make sure to avoid swipping with self
+        static void SelectionSort(int[] scores)
+        {
+            //Selection Sort
+            int LowestScorePosition = 0;
+            int temp = 0;
+            for (int i = 0; i < scores.Length; i++)
+            {
+                LowestScorePosition = i;
+
+                for (int j = i; j < scores.Length; j++)
+                {
+                    if (scores[j] < scores[LowestScorePosition])
+                    {
+                        LowestScorePosition = j;
+                    }
+                }
+                if (LowestScorePosition != i)
+                {
+                    temp = scores[i];
+                    scores[i] = scores[LowestScorePosition];
+                    scores[LowestScorePosition] = temp;
+                }
+            }
+
+        }
+            // Insertion Sort
+            static void InsertionSort(int[] MathScores) {
+                int Temp = 0;
+                int LowestPosition = 0;
+
+                for (int i = 1; i < MathScores.Length; i++)
+                {
+                    Temp = MathScores[i];
+                    LowestPosition = i;
+                    while(LowestPosition > 0 && MathScores[LowestPosition - 1] > Temp)
+                    {
+                        MathScores[LowestPosition] = MathScores[LowestPosition -1];
+                        LowestPosition--;
+                    }
+                    MathScores[LowestPosition] = Temp; 
+                }
+            }
+        
+    static void Main(string[] args)
+    {
+        int[] scores = { 80, 75, 90, 60, 55, 89, 92, 98, 99 };
+
+        int[] MathScores = { 60, 55, 89, 92, 98, 99, 90, 75, 80 };
+
+        SelectionSort(scores);
+        InsertionSort(MathScores);
+        
+            Console.WriteLine("Selection Sorted in Ascending order: ");
+            foreach (int i in scores)
+            {
+                Console.Write(i + "  ");
+            }
+
+         
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Insertion Sorted in Ascending order: ");
+            foreach(int i in MathScores)
+            {
+                Console.Write(i + "  ");
+
+            }
+        }
+    }
+}
---
+++ Week_7_Assignment_7.1.2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using Microsoft.VisualBasic;
+
+namespace Week_7_Assignment_7._1._2
+{
+    internal class Program
+    {
+        //You are given two strings word1 and word2.Merge the strings by adding letters in alternating order, 
+        //starting with word1.If a string is longer than the other, 
+        //append the additional letters onto the end of the merged string.
+        //Return the merged string.
+
+        //O(n)
+        public static string MergeWords(string word1, string word2)
+        {
+            var sb = new StringBuilder();
+
+            int i = 0; //
+          
+
+            // Alternately
+            while (i < word1.Length || i < word2.Length)
+            {
+                if (i < word1.Length)
+                {
+                    sb.Append(word1[i]);
+
+                }
+                if (i < word2.Length)
+                {
+                    sb.Append(word2[i]);
+                  
+                }
+                i++;
+            }
+
+            // Convert to Char array
+            char[] chars = sb.ToString().ToCharArray();
+
+            //O(n^2)
+            // Selection Sort
+            for (int j = 0; j < chars.Length; j++)
+            {
+                int minpos = j;
+                for(int k = j; k < chars.Length; k++)
+                {
+                    if ((chars[k] < chars[minpos]))
+                        minpos = k;
+                }
+               
+
+                //Swap
+                char temp = chars[j];
+                chars[j] = chars[minpos];
+                chars[minpos] = temp;
+            }
+            return sb.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            string word1 = "abc";
+            string word2 = "pqr";
+
+            string merged = MergeWords(word1, word2);
+            Console.WriteLine(merged);
+            
+        }
+    }
+}
